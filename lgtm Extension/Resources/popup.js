001291b642bdee6b9e43413091e6ad105ea7c226
Rafał Grodziński <rafal.grodzinski@dely.jp>
2022-03-22 16:39:21 +0900

    Reload button

--- lgtm Extension/Resources/popup.js
+++ lgtm Extension/Resources/popup.js
@@ -1,21 +1,18 @@
 window.addEventListener("load", (event) => {
-    /*
-    //console.log("loaded");
-    var button = document.getElementById("hello_btn");
-    //console.log(button);
+    var button = document.getElementById("reload");
     button.addEventListener("click", (e) => {
-        //console.log("pressed");
-        //alert("Hello");
-        console.log("button pressed");
-        browser.tabs.query({currentWindow: true, active: true}, function (tabs) {
-            console.log("tab found");
-            browser.tabs.sendMessage(tabs[0].id, "Hello from popup");
-        });
-    });*/
+        reload();
+    });
 
+    reload();
+});
+
+function reload() {
     let request = new XMLHttpRequest();
     request.onload = function() {
         let response = JSON.parse(this.responseText);
+        let container = document.getElementById("container");
+        container.innerHTML = "";
         response.images.forEach(function (value, index, array) {
             let img = lgtmImage(value.url);
             img.addEventListener("click", (e) => {
@@ -23,13 +20,12 @@ window.addEventListener("load", (event) => {
                     browser.tabs.sendMessage(tabs[0].id, url);
                 });
             });
-            let container = document.getElementById("container");
             container.appendChild(img);
         });
     }
     request.open("GET", "https://lgtmoon.herokuapp.com/api/images/random");
     request.send();
-});
+}
 
 function lgtmImage(url) {
     let imgContainer = document.createElement("div");
