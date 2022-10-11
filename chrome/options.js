b3de19e54da435e10630781b3259d6919cfca6b8
Rafał Grodziński <rafal@rafal.grodzinski.com>
2022-10-11 22:20:13 +0900

    Persist options

--- chrome/options.js
+++ chrome/options.js
@@ -1,4 +1,26 @@
 window.addEventListener("load", event => {
     let shouldInsert = document.getElementById("shouldInsert");
     let shouldUseMarkdown = document.getElementById("shouldUseMarkdown");
+
+    chrome.storage.sync.get("shouldInsert", data => {
+        let flag = true; // Check by default
+        if ("shouldInsert" in data) {
+            flag = data.shouldInsert
+        }
+        shouldInsert.checked = flag;
+    });
+    chrome.storage.sync.get("shouldUseMarkdown", data => {
+        let flag = true; // Check by default
+        if ("shouldUseMarkdown" in data) {
+            flag = data.shouldUseMarkdown
+        }
+        shouldUseMarkdown.checked = flag;
+    });
+
+    shouldInsert.addEventListener("change", event => {
+        chrome.storage.sync.set({"shouldInsert": event.currentTarget.checked});
+    });
+    shouldUseMarkdown.addEventListener("change", event => {
+        chrome.storage.sync.set({"shouldUseMarkdown": event.currentTarget.checked});
+    });
 });
